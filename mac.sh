22c36752e12bb939da101087a0e267247a20eea0
Kory Prince <korylprince@gmail.com>
2014-11-18 12:45:01 -0600

    update brew so no installs fail

--- mac.sh
+++ mac.sh
@@ -7,6 +7,8 @@ git config --global user.email "korylprince@gmail.com"
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
+brew update
+
 brew install zsh
 brew install ctags
 brew install vim --with-lua
