c862723b0001ca24379937b80bd88667713b7b9d
Kory Prince <korylprince@gmail.com>
2016-07-13 22:27:31 -0500

    fix trackpad settings

--- .config/awesome/startup.py
+++ .config/awesome/startup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 import os
 
-every = ['synclient HorizTwoFingerScroll=1'
-         ' TapButton2=2',
+every = ['synclient HorizTwoFingerScroll=1 TapButton2=2 VertScrollDelta=-99 HorizScrollDelta=-99',
          'python /home/administrator/.config/awesome/getnasa.py',
          'xmodmap -e "keycode  6 = Shift_L"']
 once = [('dropboxd', 'dropbox'), 'xcompmgr', 'batterymon']
