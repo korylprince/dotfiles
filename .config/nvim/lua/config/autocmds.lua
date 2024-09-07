9b05c2019ff5cc4cc5bbbcd922a089d698e91fbe
Kory Prince <kory.prince@airbnb.com>
2024-09-06 20:51:45 -0500

    added shell autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "sh",
+	callback = function(event)
+		vim.keymap.set(
+			"n",
+			"<F12>",
+			'<cmd>TermExec direction=float cmd="sh %" name="shell" <CR>',
+			{ buffer = event.buf }
+		)
+	end,
+})
